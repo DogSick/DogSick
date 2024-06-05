c671066bc81683d83cc406b14b4e8fc1901cb988
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-05 17:48:38 +0900

    feature/map - map initial version modify (#4)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:dogsick_project/mapMain.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() async{
-  await dotenv.load(fileName: ".env");
-  WidgetsFlutterBinding.ensureInitialized();
-  dotenv.env['naver_map_api'];
-
-  String mapId = dotenv.get('naver_map_api');
-  await NaverMapSdk.instance.initialize(
-    clientId: mapId,
-    onAuthFailed: (ex) {
-      debugPrint("********* 네이버맵 인증오류 : $ex *********");
-    },
-  );
+void main() {
   runApp(const MyApp());
 }
 
@@ -23,9 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MapMain(),
-    );
+    return Container();
   }
 }
