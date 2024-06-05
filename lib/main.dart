29be3b27f420226e806950ffb8ad75efb2baf912
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-06-05 17:17:36 +0900

    [FEATURE][EMERGENCY] EMERGENCY PAGE VIEW EDIT (#6)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dogsick_project/emergency.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,10 +6,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const String _title = 'Widget Example';
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: _title,
+      initialRoute: '/',
+      routes: {'/': (context) => Emergency()},
+    );
   }
 }
