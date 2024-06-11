860ab14400894b6baeca7ee210a4219d947b204d
eunseongjo <tkakfkekdk4@gmail.com>
2024-06-11 11:31:28 +0900

    debug check false commit

--- lib/firstScreen.dart
+++ lib/firstScreen.dart
@@ -11,6 +11,7 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         theme: ThemeData(fontFamily: 'Pretendard'),
         home: Scaffold(
           backgroundColor: Colors.white,
