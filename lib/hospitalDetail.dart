22fe45378602650c76e4bc0308399fe9091a28e2
박진현 <emem2080@naver.com>
2024-06-12 15:46:23 +0900

    [FEATURE][HOSPITALDETAIL] commit

--- lib/hospitalDetail.dart
+++ lib/hospitalDetail.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:dogsick_project/mapMain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HospitalDetail extends StatelessWidget {
   final String bplcnm;
@@ -10,59 +13,62 @@ class HospitalDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        scrolledUnderElevation: 0,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        leading: BackButton(
-          color: Color(0xff63C54A),
-        ),
-        title: Column(
-          children: [
-            Text(
-              'Location',
-              style: TextStyle(fontSize: 12),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          scrolledUnderElevation: 0,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          leading: BackButton(
+            color: Color(0xff63C54A),
+          ),
+          title: Column(
+            children: [
+              Text(
+                'Location',
+                style: TextStyle(fontSize: 12),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/icon_location.png'),
+                  Text(
+                    '강남구 신사동 115-8',
+                    style: TextStyle(fontSize: 10),
+                  ),
+                ],
+              )
+            ],
+          ),
+          actions: [
+            SizedBox(
+              width: 60,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/icon_location.png'),
-                Text(
-                  '강남구 신사동 115-8',
-                  style: TextStyle(fontSize: 10),
-                ),
-              ],
-            )
           ],
         ),
-        actions: [
-          SizedBox(
-            width: 60,
-          ),
-        ],
-      ),
-      body: FutureBuilder(
-        future: getJsonData(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('No data available'));
-          } else {
-            var data = snapshot.data as List<dynamic>;
-            var hospital = data.firstWhere(
-                (element) => element['bplcnm'] == bplcnm,
-                orElse: () => null);
-            if (hospital == null) {
-              return Center(child: Text('병원을 찾을 수 없습니다'));
+        body: FutureBuilder(
+          future: getJsonData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData) {
+              return Center(child: Text('No data available'));
             } else {
-              return buildBody(hospital);
+              var data = snapshot.data as List<dynamic>;
+              var hospital = data.firstWhere(
+                  (element) => element['bplcnm'] == bplcnm,
+                  orElse: () => null);
+              if (hospital == null) {
+                return Center(child: Text('병원을 찾을 수 없습니다'));
+              } else {
+                return buildBody(hospital);
+              }
             }
-          }
-        },
+          },
+        ),
       ),
     );
   }
@@ -76,6 +82,7 @@ class HospitalDetail extends StatelessWidget {
 
   Widget buildBody(Map<String, dynamic> hospital) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Stack(
         children: [
           Image.asset(
@@ -169,7 +176,19 @@ class HospitalDetail extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           ElevatedButton(
-                            onPressed: () {},
+                            onPressed: () async {
+                              var url =
+                                  'nmap://navigation?dlat=${hospital['y'].toString().trim()}&dlng=${hospital['x'].toString().trim()}&dname=${hospital['bplcnm']}&appname=com.example.appname';
+                              if (await canLaunchUrl(Uri.parse(url))) {
+                                await launchUrl(Uri.parse(url));
+                              } else {
+                                var store = Platform.isIOS
+                                    ? 'https://apps.apple.com/kr/app/naver-map-navigation/id311867728'
+                                    : 'https://play.google.com/store/apps/details?id=com.nhn.android.nmap&hl=ko-KR';
+
+                                await launchUrl(Uri.parse(url));
+                              }
+                            },
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Color(0xff404A3C),
                             ),
@@ -183,10 +202,18 @@ class HospitalDetail extends StatelessWidget {
                           ),
                           SizedBox(width: 40),
                           ElevatedButton(
-                            onPressed: () {},
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Color(0xff63C54A),
                             ),
+                            // 비동기 함수 선언
+                            onPressed: () async {
+                              final url = Uri.parse('tel:${'sitetel'}');
+                              if (await canLaunchUrl(url)) {
+                                await launchUrl(url);
+                              } else {
+                                print("Can`t launch $url");
+                              }
+                            },
                             child: Text(
                               '전화하기',
                               style: TextStyle(
--- lib/mapMain.dart
+++ lib/mapMain.dart
@@ -9,7 +9,8 @@ class MapMain extends StatefulWidget {
   MapMain({Key? key}) : super(key: key);
 
   NaverMapViewOptions options = const NaverMapViewOptions(
-    initialCameraPosition: NCameraPosition(target: NLatLng(37.4988, 127.0267), zoom: 16),
+    initialCameraPosition:
+        NCameraPosition(target: NLatLng(37.4988, 127.0267), zoom: 16),
     mapType: NMapType.basic,
     locationButtonEnable: true,
   );
@@ -19,7 +20,6 @@ class MapMain extends StatefulWidget {
 }
 
 class _MapMainState extends State<MapMain> {
-
   bool _showBottomSheet = false;
 
   String result = "";
@@ -30,7 +30,6 @@ class _MapMainState extends State<MapMain> {
     // TODO: implement initState
     super.initState();
     data = new List.empty(growable: true);
-
   }
 
   @override
@@ -63,105 +62,117 @@ class _MapMainState extends State<MapMain> {
           ),
         ],
       ),
-
       body: Stack(
         children: <Widget>[
-        NaverMap(
-        // 카메라 위치 설정
-        options: const NaverMapViewOptions(
-          initialCameraPosition: NCameraPosition(target: NLatLng(37.4988, 127.0267),
-              zoom: 16,
-              bearing: 0,
-              tilt: 0
+          NaverMap(
+            // 카메라 위치 설정
+            options: const NaverMapViewOptions(
+              initialCameraPosition: NCameraPosition(
+                  target: NLatLng(37.4988, 127.0267),
+                  zoom: 16,
+                  bearing: 0,
+                  tilt: 0),
+              mapType: NMapType.basic,
+              activeLayerGroups: [
+                NLayerGroup.transit,
+                NLayerGroup.building,
+              ],
+              minZoom: 10,
+              maxZoom: 20,
+              maxTilt: 30,
+              extent: NLatLngBounds(
+                  southWest: NLatLng(31.43, 122.37),
+                  northEast: NLatLng(44.35, 132.0)),
+              locationButtonEnable: true,
+              scaleBarEnable: true,
+            ),
+            onMapReady: (controller) async {
+              print("네이버 맵 로딩됨!");
+              //
+
+              final locationOverlay = await controller.getLocationOverlay();
+              locationOverlay.setPosition(NLatLng(37.4988, 127.0267));
+              print(locationOverlay.getPosition());
+              // 오버레이 설정(원하는 위치에 마킹)
+              final location = NMarker(
+                id: 'location',
+                position: const NLatLng(37.4988, 127.0267),
+                anchor: const NPoint(0.5, 0.5),
+                size: const Size(20, 20),
+                // iconTintColor: Colors.green,
+                icon: const NOverlayImage.fromAssetImage(
+                    'assets/images/location_mark.png'),
+              );
+
+              final marker = NMarker(
+                id: NLatLng(37.4998, 127.0271).toString(),
+                position: const NLatLng(37.4998, 127.0271),
+                anchor: const NPoint(0.5, 0.5),
+                size: const Size(60, 60),
+                // iconTintColor: Colors.green,
+                icon: const NOverlayImage.fromAssetImage(
+                    'assets/images/marker.png'),
+              );
+              controller.addOverlay(location);
+              controller.addOverlay(marker);
+              // controller.addOverlayAll(markers)
+
+              marker.setOnTapListener((NMarker marker) {
+                // 마커를 클릭했을 때 실행할 코드
+                print(marker);
+                setState(() {
+                  this._showBottomSheet = true;
+                });
+                // this._showBottomSheet = !this._showBottomSheet;
+                print(this._showBottomSheet);
+              });
+            },
+            onMapTapped: (NPoint point, NLatLng latLng) {
+              // 지도를 클릭했을 때 실행할 코드
+              setState(() {
+                this._showBottomSheet = false;
+              });
+              print(this._showBottomSheet);
+            },
           ),
-          mapType: NMapType.basic,
-          activeLayerGroups: [
-            NLayerGroup.transit,
-            NLayerGroup.building,
-          ],
-          minZoom: 10,
-          maxZoom: 20,
-          maxTilt: 30,
-          extent: NLatLngBounds(southWest: NLatLng(31.43, 122.37), northEast: NLatLng(44.35, 132.0)),
-          locationButtonEnable: true,
-          scaleBarEnable: true,
-        ),
-        onMapReady: (controller) async{
-          print("네이버 맵 로딩됨!");
-          //
-
-          final locationOverlay = await controller.getLocationOverlay();
-          locationOverlay.setPosition(NLatLng(37.4988, 127.0267));
-          print(locationOverlay.getPosition());
-          // 오버레이 설정(원하는 위치에 마킹)
-          final location = NMarker(id: 'location',
-            position: const NLatLng(37.4988, 127.0267),
-            anchor: const NPoint(0.5, 0.5),
-            size: const Size(20, 20),
-            // iconTintColor: Colors.green,
-            icon: const NOverlayImage.fromAssetImage('assets/images/location_mark.png'),
-          );
-
-
-          final marker = NMarker(id: NLatLng(37.4998, 127.0271).toString(),
-            position: const NLatLng(37.4998, 127.0271),
-            anchor: const NPoint(0.5, 0.5),
-            size: const Size(60, 60),
-            // iconTintColor: Colors.green,
-            icon: const NOverlayImage.fromAssetImage('assets/images/marker.png'),
-          );
-          controller.addOverlay(location);
-          controller.addOverlay(marker);
-          // controller.addOverlayAll(markers)
-
-          marker.setOnTapListener((NMarker marker) {
-            // 마커를 클릭했을 때 실행할 코드
-            print(marker);
-            setState(() {
-              this._showBottomSheet = true;
-            });
-            // this._showBottomSheet = !this._showBottomSheet;
-            print(this._showBottomSheet);
-          });
-        },
-          onMapTapped: (NPoint point, NLatLng latLng) {
-          // 지도를 클릭했을 때 실행할 코드
-            setState(() {
-              this._showBottomSheet = false;
-            });
-            print(this._showBottomSheet);
-          },
-      ),
-      Container(
-        alignment: Alignment.topCenter,
-        margin: EdgeInsets.only(top: 20),
-        child: ElevatedButton.icon(
-        onPressed: () {
-
-        },
-          label: Text('현재 위치에서 재검색', style: TextStyle(color: Colors.black),
+          Container(
+            alignment: Alignment.topCenter,
+            margin: EdgeInsets.only(top: 20),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              label: Text(
+                '현재 위치에서 재검색',
+                style: TextStyle(color: Colors.black),
+              ),
+              icon: Icon(
+                Icons.replay_outlined,
+                size: 25,
+                color: Colors.black,
+                textDirection: TextDirection.rtl,
+              ),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+            ),
           ),
-          icon: Icon(Icons.replay_outlined, size: 25, color: Colors.black, textDirection: TextDirection.rtl,),
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-        ),
-        ),
           Container(
             alignment: Alignment.bottomCenter,
             margin: EdgeInsets.only(bottom: 40),
             child: ElevatedButton(
-              onPressed: () {  
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => SearchHospital()),);
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SearchHospital()),
+                );
               },
-              child: Text('병원목록', style: TextStyle(color: Colors.white),
+              child: Text(
+                '병원목록',
+                style: TextStyle(color: Colors.white),
               ),
-              style: ElevatedButton.styleFrom(backgroundColor: Color(0xff404a3c),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xff404a3c),
               ),
             ),
           ),
-          this._showBottomSheet
-              ? HospitalInfoWidget()
-              : Container(),
+          this._showBottomSheet ? HospitalInfoWidget() : Container(),
         ],
       ),
     );
@@ -177,20 +188,21 @@ class HospitalInfoWidget extends StatelessWidget {
       right: 50,
       left: 50,
       bottom: 0,
-        child: Container(
-          alignment: Alignment.bottomCenter,
-          margin: EdgeInsets.only(bottom: 40),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(20),
-            ),
+      child: Container(
+        alignment: Alignment.bottomCenter,
+        margin: EdgeInsets.only(bottom: 40),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
           ),
-          width: 200,
-          height: 100.0,
-          // child: Column(
-          //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // ),
         ),
+        width: 200,
+        height: 100.0,
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // ),
+      ),
     );
   }
 }
